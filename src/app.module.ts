0eeb3ca6f1e83499ec6aed494f464a96411e2030
lhss <lucas.henrique0023@gmail.com>
2023-08-15 16:40:49 -0300

    feat: Configuring TypeORM and UserEntity

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,10 +15,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       password: process.env.DB_PASSWORD,
       port: Number(process.env.DB_PORT),
       username: process.env.DB_USERNAME,
+      synchronize: true,
+      entities: [`${__dirname}/**/*.entity{.js,.ts}`],
     }),
     UserModule,
   ],
   controllers: [],
   providers: [],
 })
-export class AppModule { }
+export class AppModule {}
