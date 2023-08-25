637b2febf912e0171af9c1aed787b98be5206ee2
lhss <lucas.henrique0023@gmail.com>
2023-08-25 16:37:50 -0300

    fix: migrations adjustments

---
+++ src/migration/1692989960834-insert-status.ts
@@ -0,0 +1,13 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class InsertStatus1692989960834 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`INSERT INTO public.payment_status(id, name) VALUES (1, 'Done');`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.query(`DELETE FROM public.payment_status WHERE id = 1;`);
+    }
+
+}
